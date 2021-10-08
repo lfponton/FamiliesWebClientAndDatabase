5063913efa63777ff8bc0c915968bd2a0f421ba4
Luis <lfponton@gmail.com>
2021-10-08 13:19:28 +0200

    Added Families and Adults data interfaces

---
+++ FamiliesManager/Data/IAdultsDataManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Models;
+
+namespace FamiliesManager.Data
+{
+    public interface IAdultsDataManager
+    {
+        IList<Adult> GetAdults();
+        void AddAdult(Adult adult);
+        void RemoveAdult(Adult adult);
+        void UpdateAdult(Adult adult);
+        Adult GetAdult(int id);
+    }
+}
---
+++ FamiliesManager/Data/IFamiliesDataManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Models;
+
+namespace FamiliesManager.Data
+{
+    public interface IDataManager
+    {
+        IList<Family> GetFamilies();
+        void AddFamily(Family family);
+        void RemoveFamily(Family family);
+        void UpdateFamily(Family family);
+        Family getFamily(int id);
+    }
+}
